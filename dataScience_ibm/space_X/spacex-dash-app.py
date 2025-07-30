b2f7f55ba12e5f2c7f81898b3e293ccefe9445ce
Jihyeon Choung <wldhgus67@naver.com>
2025-07-30 13:44:46 +0200

    space X-dash

--- dataScience_ibm/space_X/spacex-dash-app.py
+++ dataScience_ibm/space_X/spacex-dash-app.py
@@ -20,8 +20,7 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                         style={'textAlign': 'center', 'color': '#503D36',
                                                'font-size': 40}),
                                 # TASK 1: Add a dropdown list to enable Launch Site selection
-                                # The default select value is for ALL sites                               
-                                
+                                # The default select value is for ALL sites
                                 dcc.Dropdown(id='site-dropdown',
                                                 options=[{'label': 'All Sites', 'value': 'ALL'}]+
                                                         [{'label': site, 'value': site} for site in launch_sites],
@@ -38,7 +37,10 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
 
                                 html.P("Payload range (Kg):"),
                                 # TASK 3: Add a slider to select payload range
-                                #dcc.RangeSlider(id='payload-slider',...)
+                                dcc.RangeSlider(id='payload-slider',
+                                                min=0, max=10000, step=1000,
+                                                marks={0: '0', 2500: '2500', 5000: '5000', 7500: '7500', 10000: '10000'},
+                                                value=[min_payload, max_payload]),
 
                                 # TASK 4: Add a scatter chart to show the correlation between payload and launch success
                                 html.Div(dcc.Graph(id='success-payload-scatter-chart')),
@@ -78,4 +80,4 @@ def get_pie_chart(entered_site):
 
 # Run the app
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
